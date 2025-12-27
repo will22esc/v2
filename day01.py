4478b9d4fcaeb069b1c7b59798b22a75904b5bc2
willmmat <willmatthie@gmail.com>
2025-12-26 16:21:20 -0800

    complete day 1

---
+++ day01.py
@@ -0,0 +1,23 @@
+#Number One
+print (3+4) #addition
+print (3-4) #subtraction
+print(3*4) #multiplication
+print(3/4) #division
+print(3%4) #modulus
+print(3**4) #exponentiation
+print(3//4) #floor division
+
+#Number Two
+print("William")
+print("USA")
+
+#Number Three
+print(type(10)) #integer
+print(type(9.8))
+print(type(4-4j))
+print(type(['Will,Apple,Frog']))
+print(type('Will'))
+print(type('False'))
+
+
+#Examples
